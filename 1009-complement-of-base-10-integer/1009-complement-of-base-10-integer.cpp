e23bcfd7f78716857641cbc2e13a8437a201b42c
Praveen Chandra <81519686+praveenchandra01@users.noreply.github.com>
2022-02-09 15:45:55 +0530

    Time: 8 ms (6.87%), Space: 5.9 MB (40.90%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -6,7 +6,7 @@ public:
         if(n==0) return 1;
         while(m!=0){
             mask=(mask<<1)|1;
-            m=(m>>1);
+            m=m>>1;
         }
         int ans = (~n) & mask;
         return ans;
