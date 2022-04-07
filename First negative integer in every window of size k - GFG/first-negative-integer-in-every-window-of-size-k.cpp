aae7dc55b3275daea9c1f5de26c88534a09030e8
Praveen Chandra <81519686+praveenchandra01@users.noreply.github.com>
2022-04-08 00:48:32 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -31,15 +31,18 @@ int main() {
 
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
-      vector<long long> v; list<int> l;
+      vector<long long> v; list<long long> l;
       int i=0,j=0;
       while(j<N){
           if(A[j]<0) l.push_back(A[j]);
           if (j-i+1<K) j++;
-          else{
+          else if(j-i+1==K){
+              if(l.size()==0) v.push_back(0);
+              else{
               v.push_back(l.front());
               if(A[i]==l.front()) l.pop_front();
-              i++;j++;
+              }  
+            i++;j++;
           }
       }
       return v;
