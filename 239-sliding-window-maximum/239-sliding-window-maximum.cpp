2202b9db5eb4154996275074761fd462d3a5e537
Praveen Chandra <81519686+praveenchandra01@users.noreply.github.com>
2022-04-08 05:46:48 +0530

    Time: 388 ms (55.95%), Space: 189.5 MB (14.29%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -5,11 +5,11 @@ public:
     list<int> l;
     vector<int> v;
 
-    if(nums.size()==1){
-      v.push_back(1);  
-     return v;
-    }
-
+    // if(nums.size()==1){
+    //   v.push_back(1);  
+    //  return v;
+    // }
+        
     while(j<nums.size()){
 
         while(l.size()>0 && l.back()<nums[j]) l.pop_back();
